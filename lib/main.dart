c2bca723157a9614482c4ca881620dd08053875e
robzeh <jn912820@dal.ca>
2020-03-20 15:38:20 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'We Rate Dogs',
+      theme: ThemeData(brightness: Brightness.dark),
+      home: MyHomePage(title: 'We Rate Dogs'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.black87,
+      ),
+      body: Container(),
+    );
+  }
+}
